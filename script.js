a75d79c6dc91e4acf86a9dbc979106e2e504007f
Muller Pierre <mullerpierre54@gmail.com>
2023-07-31 04:26:02 -0300

    Projeto de CSS

---
+++ script.js
@@ -0,0 +1,8 @@
+// Inicio header 
+let toggle = document.querySelector('.toggle');
+let body = document.querySelector('body')
+
+toggle.addEventListener('click', function() {
+        body.classList.toggle('open');
+    })
+    // End Header
